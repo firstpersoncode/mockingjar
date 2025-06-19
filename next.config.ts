97788cd236560976107267f4e361fbe7d2d150da
firstpersoncode <fpcdatadriver@gmail.com>
2025-06-20 03:46:36 +0700

    feat(next.config): add 'lodash' to optimizePackageImports for improved performance

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // output: 'standalone',
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'example.com',
+  //       port: '',
+  //       pathname: '/images/**',
+  //     },
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'another-example.com',
+  //       port: '',
+  //       pathname: '/assets/**',
+  //     },
+  //   ],
+  // },
   experimental: {
-    optimizePackageImports: ['@mui/material', '@mui/icons-material'],
+    optimizePackageImports: ['@mui/material', '@mui/icons-material', 'lodash'],
   },
 };
 
